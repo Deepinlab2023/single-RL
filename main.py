5eb284e612c37fe20130f9dafbba94d33213c796
Deepinlab2023 <161077569+Deepinlab2023@users.noreply.github.com>
2024-07-04 09:35:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,25 @@ from util.parameters import Parameters
 from util.logger import Figure
 from util.benchmarker import Utils
 
+def main_ppo():
+    # Environment Initialization
+    env_name = 'PongNoFrameskip-v4'
+    #gpu = False
+    params = Parameters()
+    nb_episodes = params.nb_episodes  # 100000
+    batch_size = params.batch_size  # 24576
+    env = PongEnv(env_name)
+    env.reset()
 
+    agent_ppo = PPO()
+    trainer_ppo = PPOtrainer()
+
+    #train
+    result = trainer_ppo.train(env, agent_ppo, nb_episodes, batch_size)
+    np.save('PPO_training_result.npy', result)
+    print('training done')
+    env.close()
+    
 def run_experiment():
     num_trials = 5
 
@@ -44,4 +62,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+    #main_ppo()
 
