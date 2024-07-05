bac5c788521b26b6c26d603bc4ab5068357b2471
Deepinlab2023 <161077569+Deepinlab2023@users.noreply.github.com>
2024-07-05 16:57:11 -0400

    Update train.py

--- train/train.py
+++ train/train.py
@@ -15,7 +15,12 @@ class PPOtrainer:
     def train(self, env, agent, nb_episodes, batch_size):
         tester = Figure()
         params = Parameters()
-        opt = th.optim.Adam(agent.parameters(), lr=params.lr)
+        opt = th.optim.Adam([
+            {'params': agent.layers.parameters(), 'lr': params.lr},
+            {'params': agent.critic.parameters(), 'lr': params.lr_c}
+        ])
+        #opt = th.optim.Adam(agent.layers.parameters(), lr=params.lr)
+        #opt_c = th.optim.Adam(agent.critic.parameters(), lr=params.lr_c)
         reward_sum_running_avg = None
         reward_sum_running_avg_history = []
         training_results = []
@@ -23,32 +28,38 @@ class PPOtrainer:
 
         for it in range(nb_episodes):
             d_obs_history, action_history, action_prob_history, reward_history = [], [], [], []
+            state_val_history = []
+            done_history = []
             episode_rewards = 0
 
             for ep in range(params.ep):
                 obs, prev_obs = env.reset(), None
+                obs = obs[0]
                 for t in range(params.t):
                     d_obs = env.pre_process(obs, prev_obs)
 
                     with th.no_grad():
                         action, action_prob = agent(d_obs)
 
+                    state_val = agent.forward_critic(d_obs)
                     prev_obs = obs
-                    obs, reward, done, _ = env.step(agent.convert_action(action, self.env_name))
+                    obs, reward, done, truncated, _ = env.step(agent.convert_action(action, self.env_name))
 
                     d_obs_history.append(d_obs)
                     action_history.append(action)
                     action_prob_history.append(action_prob)
                     reward_history.append(reward)
+                    state_val_history.append(state_val)
+                    done_history.append(done)
 
                     episode_rewards += reward
 
                     if done:
                         reward_sum = sum(reward_history[-t:])
                         reward_sum_running_avg = 0.99 * reward_sum_running_avg + 0.01 * reward_sum if reward_sum_running_avg else reward_sum
-                        print(
-                            'Iteration %d, Episode %d (%d timesteps) - last_action: %d, last_action_prob: %.2f, reward_sum: %.2f, running_avg: %.2f' % (
-                            it, ep, t, action, action_prob, reward_sum, reward_sum_running_avg))
+                        #print(
+                        #    'Iteration %d, Episode %d (%d timesteps) - last_action: %d, last_action_prob: %.2f, reward_sum: %.2f, running_avg: %.2f' % (
+                        #    it, ep, t, action, action_prob, reward_sum, reward_sum_running_avg))
                         reward_sum_running_avg_history.append(reward_sum_running_avg)
                         break
 
@@ -58,14 +69,23 @@ class PPOtrainer:
             R = 0
             discounted_rewards = []
 
-            for r in reward_history[::-1]:
+            for r, d in zip(reward_history[::-1], done_history[::-1]):
                 if self.env_name == 'Pong-v0':
-                   if r != 0: R = 0 # scored/lost a point in pong, so reset reward sum 
+                    if r != 0: R = 0 # scored/lost a point in pong, so reset reward sum
+                if d is True:
+                    R = 0  # if terminal, R=0
                 R = r + agent.gamma * R
                 discounted_rewards.insert(0, R)
 
+            # Normalizing the rewards
             discounted_rewards = th.FloatTensor(discounted_rewards)
             discounted_rewards = (discounted_rewards - discounted_rewards.mean()) / discounted_rewards.std()
+            assert(len(discounted_rewards) == len(state_val_history))
+            advantage_history = []
+            for i_adv in range(len(discounted_rewards)):
+                adv = discounted_rewards[i_adv] - state_val_history[i_adv]
+                advantage_history.append(adv)
+            assert(len(advantage_history) == len(discounted_rewards))
 
             # update policy
             for _ in range(params.training_times):
@@ -73,21 +93,35 @@ class PPOtrainer:
                 d_obs_batch = th.cat([d_obs_history[idx] for idx in idxs], 0)
                 action_batch = th.LongTensor([action_history[idx] for idx in idxs])
                 action_prob_batch = th.FloatTensor([action_prob_history[idx] for idx in idxs])
-                advantage_batch = th.FloatTensor([discounted_rewards[idx] for idx in idxs])
+                #advantage_batch = th.FloatTensor([discounted_rewards[idx] for idx in idxs])
+                advantage_batch = th.FloatTensor([advantage_history[idx] for idx in idxs])
+                state_val_batch = th.FloatTensor([state_val_history[idx] for idx in idxs])
+                discounted_rewards_batch = th.FloatTensor([discounted_rewards[idx] for idx in idxs])
 
                 opt.zero_grad()
-                loss = agent(d_obs_batch, action_batch, action_prob_batch, advantage_batch)
+                loss_a = agent(d_obs_batch, action_batch, action_prob_batch, advantage_batch)
+                loss_c = agent.critic_loss(state_val_batch, discounted_rewards_batch)
+                loss = loss_a + loss_c
                 loss.backward()
                 opt.step()
 
             if it % params.test_interval == 0:
-                test_reward = tester.test(env, agent, self.env_name)
-                test_results.append(test_reward)
-                print('Training reward for episode %d: %.2f' % (it, test_reward))
+                # test 10 times for more accurate results
+                test_sum = 0
+                for test_i in range(params.test_trials):
+                    test_reward = tester.test(env, agent, self.env_name)
+                    test_sum += test_reward
+                test_average = test_sum / params.test_trials
+                test_results.append(test_average)
+                print('Training reward for episode %d: %.2f' % (it, test_average))
 
             if it % params.save_episode == 0:
-                # th.save(agent.state_dict(), 'params.ckpt')
-                pass
+                if it == 0:
+                    th.save(agent.state_dict(), 'params.ckpt')
+                else:
+                    if test_average >= max(test_results[:-1]):
+                        th.save(agent.state_dict(), 'params.ckpt')
+                #pass
 
         return training_results, test_results
 
