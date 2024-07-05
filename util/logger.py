8be8ee8e179f4a0e993353ed22844fed89b68a4b
Deepinlab2023 <161077569+Deepinlab2023@users.noreply.github.com>
2024-07-05 16:57:42 -0400

    Update logger.py

--- util/logger.py
+++ util/logger.py
@@ -10,6 +10,7 @@ from util.parameters import Parameters
 class Figure:
     def test(self, env, agent, env_name):
         obs, prev_obs = env.reset(), None
+        obs = obs[0]
         reward_sum = 0
         reward_history = []
         params = Parameters()
@@ -20,7 +21,7 @@ class Figure:
                 action, action_prob = agent(d_obs)
 
             prev_obs = obs
-            obs, reward, done, _ = env.step(agent.convert_action(action, env_name))
+            obs, reward, done, truncated, _ = env.step(agent.convert_action(action, env_name))
 
             reward_sum += reward
             reward_history.append(reward)
