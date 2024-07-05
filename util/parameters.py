5f9c2f59777d8cdca388505a9904095080e4c364
Deepinlab2023 <161077569+Deepinlab2023@users.noreply.github.com>
2024-07-05 16:58:17 -0400

    Update parameters.py

--- util/parameters.py
+++ util/parameters.py
@@ -1,16 +1,19 @@
 class Parameters:
     def __init__(self):
-        self.nb_episodes = 1000
-        self.batch_size = 64
+        self.nb_episodes = 1000  # or 500
+        self.batch_size = 128  # or 64
         self.gamma = 0.99
         self.eps_clip = 0.2
         self.layers1_num = 4  # CartPole state space dimension
-        self.layers2_num = 128
+        self.layers2_num = 64  # or 128, hidden_layer
         self.out_num = 2  # CartPole action space dimension
-        self.lr = 3e-4
+        self.lr = 3e-4  # learning rate for actor network
+        self.lr_c = 0.001  # learning rate for critic network
         self.ep = 10
-        self.t = 1000
+        self.t = 500  # or 1000 the max time steps
         self.training_times = 10
         self.save_episode = 50
         self.test_episode = 25
-        self.test_interval = 10
+        self.test_trials = 10  # test 10 times and get the average result
+        self.test_interval = 10  # test every 10 episodes
+        self.num_trials = 5
